47a1a3feadf0078abc5596b94f4d52e7f9f76237
marko delgadillo <markoneseven@gmail.com>
2017-11-07 13:57:51 -0800

    #1 Fetched image source data from database to match image tag id.

--- techi/main.js
+++ techi/main.js
@@ -2,29 +2,51 @@ function addImage() {
   fetch('https://techi.envivent.com/images.json')
     .then(res => res.json())
     .then(data => (data = data.employees))
-    .then(data => renderEmployeeImage(data))
+    .then(data => getEmployeeImage(data))
+}
+
+function getEmployeeImage(data) {
+  data.forEach(function(employee) {
+    let id = employee.id
+    let src = employee.full
+    renderEmployeeImage(id, src)
+  })
+}
+
+function renderEmployeeImage(id, src) {
+  let images = document.querySelectorAll('.left-img')
+  images.forEach(function(image) {
+    console.log(image.id)
+    console.log(src)
+    console.log(image.src)
+    if (image.id == id) {
+      image.src = 'https://techi.envivent.com/employees/' + src
+    }
+  })
 }
 
 function addName() {
   fetch('https://techi.envivent.com/names.json')
     .then(res => res.json())
     .then(data => (data = data.employees))
-    .then(data => renderEmployeeName(data))
+    .then(data => getEmployeeName(data))
 }
+
+function getEmployeeName(data) {
+  data.forEach(function(employee) {
+    let name = employee.first_name + ' ' + employee.last_name
+    let id = employee.id
+    renderEmployeeName(id, name)
+  })
+}
+
+function renderEmployeeName(id, name) {}
 
 function addDescription() {
   fetch('https://techi.envivent.com/description.json')
     .then(res => res.json())
     .then(data => (data = data.employees))
     .then(data => renderEmployeeDesc(data))
-}
-
-function renderEmployeeImage(data) {
-  data.forEach(function(employee) {
-    if (employee.id === 1) {
-      console.log(employee.full)
-    }
-  })
 }
 
 function renderEmployeeName(data) {
@@ -42,3 +64,5 @@ function renderEmployeeDesc(data) {
     }
   })
 }
+
+addImage()
